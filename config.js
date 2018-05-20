d400d6083d8e38284e5030793da242a1120a3a20
nqvst <jack.enqvist@gmail.com>
2018-05-20 11:31:19 +0200

    add skeleton and dependencies

---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const homeFolder = process.env['HOME'];
+const configFilePath = homeFolder + '/.hidden';
+
+function save(content) {
+    if(exists()) {
+        console.warn('config file already created.')
+        return;
+    }
+    fs.writeFileSync(content);
+}
+
+function get() {
+    const configBuffer = fs.readSync(configFilePath);
+    console.log(configBuffer.toString());
+}
+
+function exists() {
+    return fs.existsSync(configFilePath);
+}
+
+
+moduel.exports = {
+    get,
+    save,
+}
