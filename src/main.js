6bc029e861ee27f723b18d5509e396c956e12410
nqvst <nqvst@users.noreply.github.com>
2021-03-20 21:48:44 +0100

    fixed somethings but its still kind of broken

--- src/main.js
+++ src/main.js
@@ -1,70 +1,76 @@
 const {
-    app,
-    BrowserWindow,
-    Menu,
-    ipcMain: ipc,
-    globalShortcut,
-    remote,
-} = require('electron');
-const path = require('path');
-const url = require('url');
-const config = require('./util/config');
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain: ipc,
+  globalShortcut,
+  remote,
+} = require("electron");
+const path = require("path");
+const url = require("url");
+const config = require("./util/config");
 
-const mainMenuTemplate = require('./menu/mainMenu.js');
+const mainMenuTemplate = require("./menu/mainMenu.js");
 
 let win;
 
 function createMainMenu() {
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(mainMenu);
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 }
 
 function createWindow() {
+  win = new BrowserWindow({
+    width: 600,
+    height: 100,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
 
-    win = new BrowserWindow({ width: 600, height: 100, frame: false });
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "templates/index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'templates/index.html'),
-        protocol: 'file:',
-        slashes: true,
-    }));
+  win.webContents.openDevTools();
 
-    win.webContents.openDevTools();
+  win.on("close", (e) => {
+    win = null;
+  });
 
-    win.on('close', (e) => {
+  const ret = globalShortcut.register("Control+Command+L", () => {
+    app.focus();
+  });
 
-        win = null;
+  if (!ret) {
+    console.log("registration failed");
+  }
 
-        // e.preventDefault();
-        // win.hide();
-      });
+  console.log(globalShortcut.isRegistered("Control+Command+L"));
 
-    const ret = globalShortcut.register('Control+Command+P', () => {
-        app.focus();
-    });
-
-    if (!ret) {
-        console.log('registration failed')
-    }
-
-    console.log(globalShortcut.isRegistered('Control+Command+P'));
-
-    // createMainMenu();
+  // createMainMenu();
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
 });
 
-app.on('will-quit', () => {
-    globalShortcut.unregisterAll();
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll();
 });
 
-ipc.on('setup-done', () => {
-    console.log('setup-done in main.js')
-    win.webContents.send('reload-main');
-})
+ipc.on("setup-done", () => {
+  console.log("setup-done in main.js");
+  win.webContents.send("reload-main");
+});
--- src/templates/welcome.js
+++ src/templates/welcome.js
@@ -1,126 +1,123 @@
-const {
-    app,
-    remote,
-    ipcRenderer: ipc,
-} = require('electron');
+const { app, remote, ipcRenderer: ipc } = require("electron");
 
-const path = require('path');
-const url = require('url');
-const core = require('../util/crypto');
-const config = require('../util/config');
+const path = require("path");
+const url = require("url");
+const core = require("../util/crypto");
+const config = require("../util/config");
+const words = require("../util/words");
 
-const setupBtn = document.querySelector('#setupBtn');
-const doneBtn = document.querySelector('#done');
-const recoverCheckbox = document.querySelector('#recover');
-const recoveryContainer = document.querySelector('#recoveryContainer');
-const recoveryInput = document.querySelector('#recoveryInput');
+const setupBtn = document.querySelector("#setupBtn");
+const doneBtn = document.querySelector("#done");
+const recoverCheckbox = document.querySelector("#recover");
+const recoveryContainer = document.querySelector("#recoveryContainer");
+const recoveryInput = document.querySelector("#recoveryInput");
 
 const MIN_PASSWORD_LENGTH = 8;
 
 const entropy = [];
 
-const validPassword = (pw1, pw2) => (
-    pw1 === pw2 && pw1.length >= MIN_PASSWORD_LENGTH
-);
+const validPassword = (pw1, pw2) =>
+  pw1 === pw2 && pw1.length >= MIN_PASSWORD_LENGTH;
 
-const mouseListener = e => {
-    if (entropy.length >= 10000) {
-        return
-    }
-    const { screenY, screenX } = e;
-    console.log(e);
-    entropy.push(screenY * screenX);
+const mouseListener = (e) => {
+  if (entropy.length >= 10000) {
+    return;
+  }
+  const { screenY, screenX } = e;
+  console.log(e);
+  entropy.push(screenY * screenX);
 };
 
-const getSeed = (recoverySeed, showRecovery) => (
-    (showRecovery && recoverySeed) ?
-        { password, recoverySeed } :
-        { password, entropy }
-);
+const getSeed = (recoverySeed, showRecovery) =>
+  showRecovery && recoverySeed ? recoverySeed : entropy;
 
-window.addEventListener('mousemove', mouseListener);
+window.addEventListener("mousemove", mouseListener);
 
 function setup(password) {
-    const recoverySeed = document.querySelector('#recoveryInput').value;
+  const recoverySeed = document.querySelector("#recoveryInput").value;
 
-    const seed = getSeed(showRecovery, recoverySeed);
+  const seed = getSeed(showRecovery, recoverySeed);
 
-    if(seed) {
-        if (seed === recoverySeed && showRecovery) {
-            done();
-            return;
-        }
-
-        const seedOutputElement = document.querySelector('#seed');
-        seedOutputElement.innerHTML = seed;
-        const cardContainer = document.querySelector('#cardContainer');
-        const setupForm = document.querySelector('#setupForm');
-
-        cardContainer.classList.toggle('hidden');
-        setupForm.classList.toggle('hidden');
+  if (seed) {
+    if (seed === recoverySeed && showRecovery) {
+      done();
+      return;
     }
+    console.log({ seed });
+
+    const seedOutputElement = document.querySelector("#seed");
+    seedOutputElement.innerHTML = seed
+      .slice(25)
+      .map((n) => words[n % words.length])
+      .join(" ");
+    const cardContainer = document.querySelector("#cardContainer");
+    const setupForm = document.querySelector("#setupForm");
+
+    cardContainer.classList.toggle("hidden");
+    setupForm.classList.toggle("hidden");
+  }
 }
 
 let showRecovery = false;
 
-recoverCheckbox.addEventListener('change', e => {
-    const { target: { checked } = {}} = e;
+recoverCheckbox.addEventListener("change", (e) => {
+  const { target: { checked } = {} } = e;
 
-    showRecovery = Boolean(checked);
-    if (showRecovery) {
-        recoveryContainer.classList.remove('hidden');
-    } else {
-        recoveryContainer.classList.add('hidden');
-    }
+  showRecovery = Boolean(checked);
+  if (showRecovery) {
+    recoveryContainer.classList.remove("hidden");
+  } else {
+    recoveryContainer.classList.add("hidden");
+  }
 });
 
-setupBtn.addEventListener('click', e => {
-    e.preventDefault();
+setupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    window.removeEventListener('mousemove', mouseListener);
+  window.removeEventListener("mousemove", mouseListener);
 
-    const pw1 = document.querySelector('#pass1').value
-    const pw2 = document.querySelector('#pass2').value
+  const pw1 = document.querySelector("#pass1").value;
+  const pw2 = document.querySelector("#pass2").value;
 
-    if (validPassword(pw1, pw2)) {
-
-        if(config.privateKeyExists()) {
-            const answer = remote.dialog.showMessageBox({
-                title: 'Overwrite',
-                type: 'question',
-                buttons: ['Cancel', 'Overwrite'],
-                defaultId: 0,
-                message: 'A secret key already exists \nDo you want to overwrite the existing secret key and loose all the passwords?',
-            });
-
-            if (answer !== 0) {
-                setup(pw1);
-            }
-
-        } else {
-            setup(pw1);
-        }
-    } else {
-        alert('password doesn\'t match.');
-    }
-});
-
-doneBtn.addEventListener('click', e => {
-    const answer = remote.dialog.showMessageBox({
-        title: 'Are you sure',
-        type: 'question',
-        buttons: ['NO Wait!', 'Yes i wrote it down!'],
+  if (validPassword(pw1, pw2)) {
+    if (config.privateKeyExists()) {
+      const answer = remote.dialog.showMessageBox({
+        title: "Overwrite",
+        type: "question",
+        buttons: ["Cancel", "Overwrite"],
         defaultId: 0,
-        message: 'Are you sure you have written it down? you will never see them again',
-    });
+        message:
+          "A secret key already exists \nDo you want to overwrite the existing secret key and loose all the passwords?",
+      });
 
-    if(answer) {
-       done();
+      if (answer !== 0) {
+        setup(pw1);
+      }
+    } else {
+      setup(pw1);
     }
+  } else {
+    alert("password doesn't match.");
+  }
+});
+
+doneBtn.addEventListener("click", (e) => {
+  const answer = remote.dialog.showMessageBox({
+    title: "Are you sure",
+    type: "question",
+    buttons: ["NO Wait!", "Yes i wrote it down!"],
+    defaultId: 0,
+    message:
+      "Are you sure you have written it down? you will never see them again",
+  });
+
+  if (answer) {
+    done();
+  }
 });
 
 function done() {
-    const win = remote.getCurrentWindow();
-    ipc.send('setup-done');
-    win.close();
+  const win = remote.getCurrentWindow();
+  ipc.send("setup-done");
+  win.close();
 }
